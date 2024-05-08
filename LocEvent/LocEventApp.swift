62a798215c8b29b1a307b003d077cf7b4f78e396
Newbornprograming <yohandeny.10@gmail.com>
2024-05-08 17:52:49 +0700

    Initial Commit

---
+++ LocEvent/LocEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocEventApp.swift
+//  LocEvent
+//
+//  Created by MacBook Air on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LocEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
